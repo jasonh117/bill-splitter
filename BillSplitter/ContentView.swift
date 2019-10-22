9208a9a5b30edbf85f4ba465e8e5df19454cb74a
Jason Hsieh <jasonh117@hotmail.com>
2019-10-22 11:17:54 -0700

    Initial Commit

---
+++ BillSplitter/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BillSplitter
+//
+//  Created by Jason Hsieh on 10/22/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
